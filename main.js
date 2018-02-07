1ef85bd34c87ab9f338898d657fa9da1b685dae1
Salman Khan <salmanpathan30@gmail.com>
2018-02-07 20:03:50 +0530

    Added conversion functionality

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const {app, BrowserWindow, Menu} = require('electron');
+const {app, BrowserWindow, Menu, ipcMain} = require('electron');
+const dialog = require('electron').dialog;
 const path = require('path');
 const url = require('url');
+const webp = require('webp-converter');
 
+let sourceFilePath, destinationFolder;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -9,7 +12,7 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 800, height: 400});
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -19,7 +22,7 @@ function createWindow () {
   }));
 
   // Open the DevTools.
-//   win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -69,3 +72,55 @@ const appMenu = Menu.buildFromTemplate([
 ]);
 
 Menu.setApplicationMenu(appMenu);
+
+ipcMain.on('onClickSelectFile', () => {
+  const callback = (data) => {
+    console.log('onClickSelectFileResult: ' + data);
+    sourceFilePath = data;
+  };
+  const options = {
+    filters: [
+      {name: 'WebP Image', extensions: ['webp']}
+    ],
+    properties: ['openFile']
+  };
+  dialog.showOpenDialog(options, callback);
+});
+
+ipcMain.on('onClickSelectFolder', () => {
+  const callback = (data) => {
+    console.log('onClickSelectFolderResult' + data);
+    destinationFolder = data+"/unnamed.jpg";
+  };
+  const options = {
+    properties: ['openDirectory']
+  };
+  dialog.showOpenDialog(options, callback);
+});
+
+ipcMain.on('onClickConvert', () => {
+  if (!sourceFilePath) {
+    dialog.showErrorBox('Error', 'Please select WebP image!');
+    return;
+  }
+
+  if (!destinationFolder) {
+    dialog.showErrorBox('Error', 'Please select destination folder!');
+    return;
+  }
+
+  webp.dwebp(sourceFilePath, destinationFolder+'/convertedFile.jpg', '-o', (status) => {
+    console.log('Conversion Status: ' + status);
+    if (status === 100) {
+      let options = {
+        title: 'Success',
+        message: 'Conversion Successful!'
+      };
+      dialog.showMessageBox({options: options});
+      sourceFilePath = null;
+      destinationFolder = null;
+      return;
+    }
+    dialog.showErrorBox('Error', 'Conversion Unsuccessful!');
+  });
+});
